63cdf6632ff0a59e8b6b61fbfb1dfdc15a0e2b30
schott-anderson <schott.anderson@hotmail.com>
2021-09-21 14:04:42 -0300

    Atualiza model e controller, e inclui dependencia do postgresql

--- src/main/java/com/hatanaka/ecommerce/checkout/resource/checkout/CheckoutResource.java
+++ src/main/java/com/hatanaka/ecommerce/checkout/resource/checkout/CheckoutResource.java
@@ -1,10 +1,12 @@
 package com.hatanaka.ecommerce.checkout.resource.checkout;
 
 import com.hatanaka.ecommerce.checkout.event.CheckoutCreatedEvent;
+import com.hatanaka.ecommerce.checkout.service.CheckoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CheckoutResource {
 
+    private final CheckoutService checkoutService;
+
      @PostMapping("/")
-    public ResponseEntity<Void> create(CheckoutRequest checkoutRequest) {
+    public ResponseEntity<Void> create(@RequestBody CheckoutRequest checkoutRequest) {
+         checkoutService.create(checkoutRequest);
          return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutServiceImpl.java
+++ src/main/java/com/hatanaka/ecommerce/checkout/service/CheckoutServiceImpl.java
@@ -3,19 +3,23 @@ package com.hatanaka.ecommerce.checkout.service;
 import com.hatanaka.ecommerce.checkout.entity.CheckoutEntity;
 import com.hatanaka.ecommerce.checkout.repository.CheckoutRepository;
 import com.hatanaka.ecommerce.checkout.resource.checkout.CheckoutRequest;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutServiceImpl implements CheckoutService{
 
     private final CheckoutRepository checkoutRepository;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
-        return Optional.empty();
+        final CheckoutEntity checkoutEntity = CheckoutEntity.builder()
+                .code(UUID.randomUUID().toString())
+                .build();
+        return Optional.of(checkoutRepository.save(checkoutEntity));
     }
 }
